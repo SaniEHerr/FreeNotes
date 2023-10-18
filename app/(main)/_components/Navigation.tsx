70aa974eb753cf6b81bf6296766cbcad8ca1a509
SaniEHerr <santiagoeherrera@gmail.com>
2023-10-18 18:05:56 -0300

    Creating de Schema and api with Convex

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -6,11 +6,15 @@ import { usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import UserItem from "./UserItem"
+import { useQuery } from "convex/react"
+import { api } from "@/convex/_generated/api"
 
 const Navigation = () => {
 
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)")
+
+  const documents = useQuery(api.documents.get);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -118,7 +122,11 @@ const Navigation = () => {
           <UserItem />
         </div>
         <div className="mt-4">
-          <p>Documents</p>
+          {documents?.map((document) => (
+            <p key={document._id}> 
+              {document.title} 
+            </p>
+          ))}
         </div>
         <div 
           onMouseDown={handleMouseDown}
