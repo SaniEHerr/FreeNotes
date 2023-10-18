605fc42a35918a0ad4952a372cb3ebf7e1a291d7
SaniEHerr <santiagoeherrera@gmail.com>
2023-10-18 16:57:22 -0300

    Auth with Clerk and setup database with Convex

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -2,8 +2,15 @@
 
 import { Button } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
+import { useConvexAuth } from "convex/react"
+import Spinner from "@/components/Spinner"
+import Link from "next/link"
+import { SignInButton } from "@clerk/clerk-react"
 
 const Heading = () => {
+
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="font-bold text-3xl sm:text-5xl md:text-6xl">
@@ -12,10 +19,27 @@ const Heading = () => {
       <h3 className="font-medium text-base sm:text-xl md:text-2xl">
         Free Notes is the connected workspace where <br /> better, faster work happens.
       </h3>
-      <Button>
-        Enter Free Notes
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Free Notes
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Free Notes for Free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   )
 }
--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -3,11 +3,17 @@
 import useScrollTop from '@/hooks/useScrollTop'
 import { cn } from '@/lib/utils';
 import Logo from './Logo';
-import { ModeToggle } from '@/components/modeToggle';
+import { ModeToggle } from '@/components/ModeToggle';
+import { useConvexAuth } from "convex/react"
+import { SignInButton, UserButton } from "@clerk/clerk-react"
+import { Button } from '@/components/ui/button';
+import Spinner from '@/components/Spinner';
+import Link from 'next/link';
 
 
 const Navbar = () => {
 
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -15,7 +21,36 @@ const Navbar = () => {
       "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-md"
     )}>
       <Logo />
-      <div className='w-full flex justify-between items-center gap-x-2 md:ml-auto md:justify-end '>
+      <div className='w-full flex justify-between items-center gap-x-2 md:ml-auto md:justify-end'>
+        {isLoading && (
+          <Spinner />
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode='modal'>
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode='modal'>
+              <Button size="sm">
+                Get Free Notes Free
+              </Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild >
+              <Link href="/documents">
+                Enter Free Notes
+              </Link>
+            </Button>
+            <UserButton 
+              afterSignOutUrl="/"
+            />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
