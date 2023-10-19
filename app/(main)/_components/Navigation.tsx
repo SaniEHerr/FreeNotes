45d2d534f1ca3949c7a5feae61e812e83714939b
SaniEHerr <santiagoeherrera@gmail.com>
2023-10-19 13:36:07 -0300

    Document list with add documents child function

--- app/(main)/_components/Navigation.tsx
+++ app/(main)/_components/Navigation.tsx
@@ -6,17 +6,18 @@ import { usePathname } from "next/navigation"
 import { ElementRef, useEffect, useRef, useState } from "react"
 import { useMediaQuery } from "usehooks-ts"
 import UserItem from "./UserItem"
-import { useQuery, useMutation } from "convex/react"
+import { useMutation } from "convex/react"
 import { api } from "@/convex/_generated/api"
 import Item from "./Item"
 import { toast } from "sonner"
+import DocumentList from "./DocumentList"
 
 const Navigation = () => {
 
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)")
 
-  const documents = useQuery(api.documents.get);
+
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
@@ -152,11 +153,7 @@ const Navigation = () => {
           />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}> 
-              {document.title} 
-            </p>
-          ))}
+          <DocumentList />
         </div>
         <div 
           onMouseDown={handleMouseDown}
