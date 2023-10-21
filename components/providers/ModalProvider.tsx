74f94e59259f6123ce734c05223fb5810e259c76
SaniEHerr <santiagoeherrera@gmail.com>
2023-10-21 03:30:31 -0300

    Cover Image. Upload, update and remove image from Document pages

--- components/providers/ModalProvider.tsx
+++ components/providers/ModalProvider.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import SettingsModal from '../modals/SettingsModal'
+import CoverImageModal from '@/components/modals/CoverImageModal'
 
 const ModalProvider = () => {
 
@@ -18,6 +19,7 @@ const ModalProvider = () => {
   return (
     <>
       <SettingsModal />
+      <CoverImageModal />
     </>
   )
 }
